b3d00f6e21884e1f7d72f0783a6c2093ed38047a
gtjames <gtjames@gmail.com>
2022-11-11 17:58:25 -0600

    This is ready for the students

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-east-1'                     // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''                              // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://b0idpws0ri.execute-api.us-east-1.amazonaws.com/dev'                              // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
