facaa842893cd58115bfbe873ac7305699f7df9f
gtjames <gtjames@gmail.com>
2022-11-14 13:01:07 -0600

    finally the last push

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-1_EojW9dDLF',          // 'us-east-2_Fd4ORxxou',  // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '6k606g3eph2rv3njpicstkn441',   // 'ria3bkikjbt7g4havdg7bvhd8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             'us-east-1'                     // e.g. us-east-2
+        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             ''          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://b0idpws0ri.execute-api.us-east-1.amazonaws.com/dev'                              // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
--- js/requestUnicorn.js
+++ js/requestUnicorn.js
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+// SPDX-License-Identifier: MIT-0
 
 const randomBytes = require('crypto').randomBytes;
 
