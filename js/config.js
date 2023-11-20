bf493864ccb590738069f94e9f126d8afa945ead
zachtidwell <zachtidwell14@gmail.com>
2023-11-19 18:58:01 -0600

    config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_jnckq9PLn',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '535v02gghblqi3n912l4uo3f2p',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
