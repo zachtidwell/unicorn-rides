cfd220b87e045060180e483dedccc4a520a9ee82
zachtidwell <zachtidwell14@gmail.com>
2023-12-08 19:32:41 -0600

    location

--- js/ride.js
+++ js/ride.js
@@ -170,15 +170,12 @@ let map;
     }
 
     function cityInfo(event){
-        console.log('cityInfo function called'); // Log when the function is called
-        var pickupLocation =  WildRydes.map.center;
-        console.log('WildRydes.map:', WildRydes.map.center); // Log the WildRydes.map object
-        console.log('pickupLocation:', pickupLocation); // Log the pickupLocation variable
-
         var location = WildRydes.map.selectedPoint;
         if (!location) {
             location = WildRydes.map.center;
         }
+
+        console.log('location:', pickupLocation); // Log the pickupLocation variable
     
         // Use OpenCage Geocoding API to get city name from coordinates
         $.ajax({
